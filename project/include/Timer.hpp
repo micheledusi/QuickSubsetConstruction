1a160be4b508e07074a18a28c7010db6fda55aad
MicheleDusi <micheledusi.618@gmail.com>
2022-12-07 09:30:21 +0100

    Extract time-measuring macro in other module

---
+++ project/include/Timer.hpp
@@ -0,0 +1,34 @@
+/*
+ * Michele Dusi, Gianfranco Lamperti
+ * Quick Subset Construction
+ * 
+ * Timer.hpp
+ *
+ * 
+ * This file contains the definition of a useful macro for time measuring.
+ * It can be used every time you want to measure the time spent to execute a block of code.
+ */
+
+namespace quicksc {
+    
+    /**
+     * Macro function that measures the time spent to execute a block of code.
+     * It stores the time in a variable (declared internally to the macro) whose name can be inserted as a parameter.
+     * 
+     * Example:
+     * 
+     * MEASURE_MILLISECONDS( time_variable ) {
+     * 		// Code to be measured
+     * }
+     * printf("Time spent: %ul ms", time_variable);
+     * 
+     */
+    #define MEASURE_MILLISECONDS( ms_result ) 											\
+        unsigned long int ms_result = 0; 												\
+        auto CONCAT( ms_result, _start ) = chrono::high_resolution_clock::now(); 		\
+        for (	int CONCAT( ms_result, _for_counter ) = 0; 								\
+                CONCAT( ms_result, _for_counter ) < 1;									\
+                CONCAT( ms_result, _for_counter++ ),									\
+                ms_result = std::chrono::duration_cast<std::chrono::milliseconds>(chrono::high_resolution_clock::now() - CONCAT( ms_result, _start )).count() )
+    
+} // namespace quicksc
