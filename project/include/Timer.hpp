e0c96f82a06ec32706798485be7659253c15572b
MicheleDusi <micheledusi.618@gmail.com>
2023-04-13 10:57:15 +0200

    Change time-measure from milli to nanoseconds

--- project/include/Timer.hpp
+++ project/include/Timer.hpp
@@ -14,7 +14,7 @@
 namespace quicksc {
     
     /**
-     * Macro function that measures the time spent to execute a block of code.
+     * Macro function that measures the time spent to execute a block of code, in milliseconds.
      * It stores the time in a variable (declared internally to the macro) whose name can be inserted as a parameter.
      * 
      * Example:
@@ -26,11 +26,31 @@ namespace quicksc {
      * 
      */
     #define MEASURE_MILLISECONDS( ms_result ) 											\
-        unsigned long int ms_result = 0; 												\
+        unsigned long long int ms_result = 0; 												\
         auto CONCAT( ms_result, _start ) = chrono::high_resolution_clock::now(); 		\
         for (	int CONCAT( ms_result, _for_counter ) = 0; 								\
                 CONCAT( ms_result, _for_counter ) < 1;									\
                 CONCAT( ms_result, _for_counter++ ),									\
                 ms_result = std::chrono::duration_cast<std::chrono::milliseconds>(chrono::high_resolution_clock::now() - CONCAT( ms_result, _start )).count() )
+    
+    /**
+     * Macro function that measures the time spent to execute a block of code, in nanoseconds.
+     * It stores the time in a variable (declared internally to the macro) whose name can be inserted as a parameter.
+     * 
+     * Example:
+     * 
+     * MEASURE_NANOSECONDS( time_variable ) {
+     * 		// Code to be measured
+     * }
+     * printf("Time spent: %ul ns", time_variable);
+     * 
+     */
+    #define MEASURE_NANOSECONDS( ns_result ) 											\
+        unsigned long long int ns_result = 0; 												\
+        auto CONCAT( ns_result, _start ) = chrono::high_resolution_clock::now(); 		\
+        for (	int CONCAT( ns_result, _for_counter ) = 0; 								\
+                CONCAT( ns_result, _for_counter ) < 1;									\
+                CONCAT( ns_result, _for_counter++ ),									\
+                ns_result = std::chrono::duration_cast<std::chrono::nanoseconds>(chrono::high_resolution_clock::now() - CONCAT( ns_result, _start )).count() )
     
 } // namespace quicksc
